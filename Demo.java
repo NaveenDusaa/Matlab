991f822c00730dafcac42814864b641f0a405b68
NaveenDusaa <naveendusa490@gmail.com>
2024-12-05 23:14:49 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,86 @@
+ import java.util.*;
+ class Demo
+
+{
+
+	public static void main(String[] args) {
+	/*	int a=50;
+		if (a>50)
+		{
+			System.out.println("greater");
+
+		}
+		else if (a<20)
+		{
+			System.out.println("lower");
+		}
+		else 
+		{
+			System.out.println("no output");  */
+        
+             
+  /*			 int marks=-10;
+			 if (marks>=80) {
+				 System.out.println("Student pass the subject Grade A ");
+			 }
+			 else if (marks<=50) {
+				 System.out.println("Student pass the subject Grade B");
+
+			 }
+			 
+			 else {
+				 System.out.println("Student fail the subjects"); */
+	/*		      int age =60;
+				 if (age<10) {
+					 System.out.println("Childrens");
+				 
+				 }
+				 else if (age<20) {
+					 System.out.println("Tenegers");
+				 
+				 }
+				 else if (age<30) {
+				      System.out.println("adults");
+				 }
+				 else {
+					 System.out.println("majors");    */
+
+
+                
+				 Scanner S=new Scanner (System.in);
+
+				 System.out.println("Enter Your Score :");
+				 int a=S.nextInt();
+				 int Lscore=355;
+				 if (a < Lscore)
+				 {
+					 System.out.println("This Match is lose ");
+				 } 
+				 else if (a > Lscore)
+				 {
+					 System.out.println("This Match is won ");
+				 }
+				 else {
+					 System.out.println("This Match is tie"); 
+				 }
+
+
+	}
+
+}
+			 
+
+
+		
+
+
+
+	  
+	  
+	
+	
+
+	
+		
+	
+
