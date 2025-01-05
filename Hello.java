fe1f2d577f47d1ea34ae66c6643734c9526e7140
NaveenDusaa <naveendusa490@gmail.com>
2025-01-05 16:51:26 +0530

    s

--- Hello.java
+++ Hello.java
@@ -1,27 +1,57 @@
-class A
-{
-	A(){
-		this(50);
-		System.out.println("one");
-	}
-	A(int a){
-		System.out.println(a);
+package Stringhandling;
+// Boxing and Unboxing ;
+
+public class Hello {
+	public static void main(String args[]) {
+		int a=20;
+		Integer b=Integer. valueOf(a);//auto boxing 
+		Integer j=a;
+		System.out.println(j);
+		
+		
+		int c=60;
+		Integer d=new Integer(c);
+		System.out.println(d);
+	
+		
+		float f=450.6f;
+		Float ff=Float.valueOf(f);
+		Float fff=ff;
+		System.out.println(fff);
+		
+		Float u=new Float(563.5f);//unboxing 
+		System.out.println(u);
+		
+		
+		char ch='D';
+		Character chh=ch;
+		System.out.println(chh);
+		
+		
+		
+		Character chr=new Character('O');//unboxing
+		char chrr=ch;
+		System.out.println(chrr);
+		
+		boolean t=true;
+		Boolean t1=Boolean.valueOf(t);
+		Boolean t2=t1;
+		System.out.println(t2);
+		
+		
+		Boolean ty=new Boolean(false);//unboxing
+		boolean tyy=ty;
+		System.out.println(tyy);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
-class Hello extends A
-{
-	Hello(){
-		this(30);
-		System.out.println("two");
-	}
-	Hello(int b){
-		System.out.println(b);
-}
-public static void main (String args[]){
-	new Hello();
-}
-}
-
-     
-
