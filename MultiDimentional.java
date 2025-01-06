f14d6a7e449cf519f38bb502ded3b84f05bcba35
NaveenDusaa <naveendusa490@gmail.com>
2025-01-06 22:34:53 +0530

    Multidimentional Array

---
+++ MultiDimentional.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class MultiDimentional {
+	public static void main(String args[]) {
+	int[][] nam={
+			{100,200,300},
+			{400,500,500},
+			{500,800},
+			{600,80}
+			};
+	int sum=0;
+	for(int i=0;i<nam.length;i++) {
+		for(int j=0;j<nam[i].length;j++) {
+	
+			System.out.print(nam[i][j]+" ");
+	}
+	System.out.println();
+	}
+	}
+}
+	
+
+	
+
+
+
